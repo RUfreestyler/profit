c44e40f809d3f53203abf2b21cc675fe09689ee5
RUfreestyler <vladmel.melnikov@yandex.ru>
2022-11-13 21:28:21 +0300

    добавлено переключение между вопросами и подсчет баллов

--- js/balliZaOtveti.js
+++ js/balliZaOtveti.js
@@ -14,74 +14,147 @@ let professions =
     ['СЕО-специалист',0]            //11
 ]
 
-let battonClick = document.getElementById('next')
-battonClick.onclick= function(){
-    
+let button = document.getElementById('next');
+let questions = document.getElementsByClassName('questionAll');
+let i = 0;
+
+button.onclick= function(){
+    if(i == 0){
+        writeQuestion1();
+    }
+    else if(i == 1){
+        writeQuestion2();
+    }
+    questions[i].classList.add('hidden');
+    i++;
+    if(i == 2){
+        showResults();
+    }
+    questions[i].classList.remove('hidden');
 }
 
-var rates = document.getElementById('answer1').value;
-var rate_value;
-if(rates =='a1'){
-    professions[0][1]+=1;
-    professions[1][1]+=1;
-    professions[2][1]+=1;
-    professions[3][1]+=1;
-    professions[4][1]+=1;   
-    professions[5][1]+=1;
-    professions[6][1]+=1;
-    professions[7][1]+=1;
-    professions[8][1]+=1;
-    professions[9][1]+=1;
-    professions[10][1]+=1;
+function showResults(){
+    button.classList.add('hidden');
+    let max = 0;
+    let indexOfMax = 0;
+    for (let i = 0; i < professions.length; i++) {
+        if(professions[i][1] > max){
+            max = professions[i][1];
+            indexOfMax = i;
+        }
+    }
+    let profession = document.getElementById('profession');
+    profession.insertAdjacentHTML('afterbegin', professions[indexOfMax][0]);
 }
-else if(rates =='a2'){
-    professions[0][1]+=1;
-    professions[1][1]+=1;
-    professions[2][1]+=1;
-    professions[3][1]+=1;
-    professions[4][1]+=1;   
-    professions[5][1]+=1;
-    professions[6][1]+=1;
-    professions[7][1]+=1;
-    professions[8][1]+=1;
-    professions[9][1]+=1;
-    professions[10][1]+=1;
+
+function getCheckedRadioValue(name) {
+        var elements = document.getElementsByName(name);
+        for (var i=0, len=elements.length; i<len; ++i)
+            if (elements[i].checked) 
+                return elements[i].value;
+    }
+    
+
+function writeQuestion1(){
+    var rates = getCheckedRadioValue('answer1');
+    if(rates =='a1'){
+        professions[0][1]+=1;
+        professions[1][1]+=1;
+        professions[2][1]+=1;
+        professions[3][1]+=1;
+        professions[4][1]+=1;   
+        professions[5][1]+=1;
+        professions[6][1]+=1;
+        professions[7][1]+=1;
+        professions[8][1]+=1;
+        professions[9][1]+=1;
+        professions[10][1]+=1;
+    }
+    else if(rates =='a2'){
+        professions[0][1]+=1;
+        professions[1][1]+=1;
+        professions[2][1]+=1;
+        professions[3][1]+=1;
+        professions[4][1]+=1;   
+        professions[5][1]+=1;
+        professions[6][1]+=1;
+        professions[7][1]+=1;
+        professions[8][1]+=1;
+        professions[9][1]+=1;
+        professions[10][1]+=1;
+    }
+    else if(rates =='a3'){
+        professions[1][1]+=1;
+        professions[2][1]+=1;   
+        professions[5][1]+=1;      
+    }
+    else if(rates =='a4'){
+        professions[3][1]+=1;   
+        professions[8][1]+=1;
+    }
+    else if(rates =='a5'){
+        professions[0][1]+=1;
+    }
+    else if(rates =='a6'){
+        professions[9][1]+=1;
+        professions[2][1]+=1;
+        professions[8][1]+=1;
+        professions[6][1]+=1;
+        professions[1][1]+=0.5;   
+    }
+    else if(rates =='a7'){
+        professions[11][1]+=1;
+        professions[0][1]+=1;
+        professions[8][1]+=1;
+        professions[6][1]+=0.5;
+    }
+    else if(rates =='a8'){
+        professions[0][1]+=1;
+        professions[1][1]+=1;
+        professions[2][1]+=1;
+        professions[3][1]+=1;
+        professions[4][1]+=1;   
+        professions[5][1]+=1;
+        professions[6][1]+=1;
+        professions[7][1]+=1;
+        professions[8][1]+=1;
+        professions[9][1]+=1;
+        professions[10][1]+=1;
+    } 
 }
-else if(rates =='a3'){
-    professions[1][1]+=1;
-    professions[2][1]+=1;   
-    professions[5][1]+=1;      
+
+function writeQuestion2(){
+
 }
-else if(rates =='a4'){
-    professions[3][1]+=1;   
-    professions[8][1]+=1;
+
+function writeQuestion3(){
+
 }
-else if(rates =='a5'){
-    professions[0][1]+=1;
+
+function writeQuestion4(){
+
 }
-else if(rates =='a6'){
-    professions[9][1]+=1;
-    professions[2][1]+=1;
-    professions[8][1]+=1;
-    professions[6][1]+=1;
-    professions[1][1]+=0.5;   
+
+function writeQuestion5(){
+
 }
-else if(rates =='a7'){
-    professions[11][1]+=1;
-    professions[0][1]+=1;
-    professions[8][1]+=1;
-    professions[6][1]+=0.5;
+
+function writeQuestion6(){
+
 }
-else if(rates =='a8'){
-    professions[0][1]+=1;
-    professions[1][1]+=1;
-    professions[2][1]+=1;
-    professions[3][1]+=1;
-    professions[4][1]+=1;   
-    professions[5][1]+=1;
-    professions[6][1]+=1;
-    professions[7][1]+=1;
-    professions[8][1]+=1;
-    professions[9][1]+=1;
-    professions[10][1]+=1;
-} 
+
+function writeQuestion7(){
+
+}
+
+function writeQuestion8(){
+
+}
+
+function writeQuestion9(){
+
+}
+
+function writeQuestion10(){
+
+}

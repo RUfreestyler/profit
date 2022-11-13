64449647208e430fde50b9bfd8f183c9b80f8fb8
RUfreestyler <vladmel.melnikov@yandex.ru>
2022-11-13 23:48:14 +0300

    Тест с костылями готов
    Нужно добавить оформление теста и больше информации в результатах тестирования

--- js/balliZaOtveti.js
+++ js/balliZaOtveti.js
@@ -14,27 +14,106 @@ let professions =
     ['СЕО-специалист',0]            //11
 ]
 
+let allQuestions = 
+[
+    [
+        [1,1,1,1,1,1,1,1,1,1,1,1],
+        [1,1,1,1,1,1,1,1,1,1,1,1],
+        [0,1,1,0,0,1,0,0,0,0,0,0],
+        [0,0,0,1,0,0,0,0,1,0,0,0],
+        [1,0,0,0,0,0,0,0,0,0,0,0],
+        [0,1,1,0,0,0,1,0,1,0,0,0],
+        [1,0,0,0,0,0,0,0,1,0,0,1],
+        [1,1,1,1,1,1,1,1,1,1,1,1]
+    ],
+    [
+        [0,0,1,1,1,0,0,0,0,1,1,0],
+        [0,0,0,0,0,0,0,0,1,1,0,1],
+        [1,0,0,0,0,0,0,0,1,0,0,1],
+        [1,0,0,0,0,0,0.5,0,1,0,0,1]
+    ],
+    [
+        [1,0,0,0,1,0,0,1,1,1,0,0],
+        [0,1,1,0,0,1,1,0,0,1,1,1],
+        [0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,1,0.5,0.5]
+    ],
+    [
+        [1,0,0,0,0,0,0,0,0,0,0,0],
+        [1,0,1,0,0,1,0,0,1,0,0,0],
+        [0,0,0,0,1,0,1,1,0,1,1,1],
+        [0,0,1,0,1,0,0,0,0,0,0,0]
+    ],
+    [
+        [1,0,1,0,0,1,0,0,1,0,1,1],
+        [0,0,0.5,1,0,0.5,1,1,0.5,1,0,0],
+    ],
+    [
+        [0,1,1,1,1,1,1,1,0,1,0,0],
+        [0,0,0,0,0,0,0,1,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,1],
+        [1,0,0,0.5,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,1,0,0]
+    ],
+    [
+        [0,1,0,0,0,0,1,1,0,0,0,0],
+        [1,0,1,1,1,1,0,0,1,1,1,1],
+        [0,0,0,0,0,0,0,0,1,0,0,0],
+    ],
+    [
+        //доработать
+        [0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0]
+    ],
+    [
+        [0,0,1,1,0,1,0,0,0,0,1,0],
+        [1,0,0,0,0,0,0,0,1,0,0,0],
+        [0,0,0,1,0,0,1,0,0,0,0,1],
+        [0,0,1,0,0,1,0,0,0.5,0,1,0]
+    ],
+    [
+        [1,0,0,0,0,0,0,1,0,0,0,0],
+        [0,1,1,1,1,1,1,0,1,1,1,1],
+        [1,1,1,1,1,1,1,1,1,1,1,1]
+    ]
+]
+
+
 let button = document.getElementById('next');
 let questions = document.getElementsByClassName('questionAll');
-let i = 0;
+let i = 1;
 
 button.onclick= function(){
-    if(i == 0){
-        writeQuestion1();
+    let exitCode = writeQuestion(i);
+    if(exitCode == -1){
+        return;
     }
-    else if(i == 1){
-        writeQuestion2();
-    }
-    questions[i].classList.add('hidden');
+
+    questions[i-1].classList.add('hidden');
     i++;
-    if(i == 2){
+    if(i == questions.length + 1){
         showResults();
     }
-    questions[i].classList.remove('hidden');
+    questions[i-1].classList.remove('hidden');
+}
+
+function writeQuestion(i){
+    let qNumber = i;
+    let answer = parseInt(getCheckedRadioValue('answer' + qNumber)) - 1;
+    if(answer === NaN){
+        return -1;
+    }
+
+    for (let j = 0; j < professions.length; j++) {
+        professions[j][1] += allQuestions[qNumber - 1][answer][j]      
+    }
+    return 0;
 }
 
 function showResults(){
     button.classList.add('hidden');
+    document.getElementById('results').classList.remove('hidden');
     let max = 0;
     let indexOfMax = 0;
     for (let i = 0; i < professions.length; i++) {
@@ -48,113 +127,8 @@ function showResults(){
 }
 
 function getCheckedRadioValue(name) {
-        var elements = document.getElementsByName(name);
-        for (var i=0, len=elements.length; i<len; ++i)
-            if (elements[i].checked) 
-                return elements[i].value;
-    }
-    
-
-function writeQuestion1(){
-    var rates = getCheckedRadioValue('answer1');
-    if(rates =='a1'){
-        professions[0][1]+=1;
-        professions[1][1]+=1;
-        professions[2][1]+=1;
-        professions[3][1]+=1;
-        professions[4][1]+=1;   
-        professions[5][1]+=1;
-        professions[6][1]+=1;
-        professions[7][1]+=1;
-        professions[8][1]+=1;
-        professions[9][1]+=1;
-        professions[10][1]+=1;
-    }
-    else if(rates =='a2'){
-        professions[0][1]+=1;
-        professions[1][1]+=1;
-        professions[2][1]+=1;
-        professions[3][1]+=1;
-        professions[4][1]+=1;   
-        professions[5][1]+=1;
-        professions[6][1]+=1;
-        professions[7][1]+=1;
-        professions[8][1]+=1;
-        professions[9][1]+=1;
-        professions[10][1]+=1;
-    }
-    else if(rates =='a3'){
-        professions[1][1]+=1;
-        professions[2][1]+=1;   
-        professions[5][1]+=1;      
-    }
-    else if(rates =='a4'){
-        professions[3][1]+=1;   
-        professions[8][1]+=1;
-    }
-    else if(rates =='a5'){
-        professions[0][1]+=1;
-    }
-    else if(rates =='a6'){
-        professions[9][1]+=1;
-        professions[2][1]+=1;
-        professions[8][1]+=1;
-        professions[6][1]+=1;
-        professions[1][1]+=0.5;   
-    }
-    else if(rates =='a7'){
-        professions[11][1]+=1;
-        professions[0][1]+=1;
-        professions[8][1]+=1;
-        professions[6][1]+=0.5;
-    }
-    else if(rates =='a8'){
-        professions[0][1]+=1;
-        professions[1][1]+=1;
-        professions[2][1]+=1;
-        professions[3][1]+=1;
-        professions[4][1]+=1;   
-        professions[5][1]+=1;
-        professions[6][1]+=1;
-        professions[7][1]+=1;
-        professions[8][1]+=1;
-        professions[9][1]+=1;
-        professions[10][1]+=1;
-    } 
-}
-
-function writeQuestion2(){
-
-}
-
-function writeQuestion3(){
-
-}
-
-function writeQuestion4(){
-
-}
-
-function writeQuestion5(){
-
-}
-
-function writeQuestion6(){
-
-}
-
-function writeQuestion7(){
-
-}
-
-function writeQuestion8(){
-
-}
-
-function writeQuestion9(){
-
-}
-
-function writeQuestion10(){
-
+    var elements = document.getElementsByName(name);
+    for (var i=0, len=elements.length; i<len; ++i)
+        if (elements[i].checked) 
+            return elements[i].value;
 }
